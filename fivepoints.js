acd405bc08ec5870eda1eaa4848d037f2e528cac
Eric Gray <ericxgray@gmail.com>
2015-03-12 15:19:48 -0700

    Days available pushing to employee array

--- fivepoints.js
+++ fivepoints.js
@@ -26,9 +26,9 @@ var FivePointsEmployeesView = Backbone.View.extend({
 	addHours: function(event){
 		var nowBox = event.currentTarget
 		if (nowBox.id[1] == 'O' || nowBox.id[1] == 'M'){
-			this.hours += 6
+			nowBox.checked == true ? this.hours += 6 : this.hours -= 6
 		} else if (nowBox.id[1] == 'C'){
-			this.hours += 7.5
+			nowBox.checked == true ? this.hours += 7.5 : this.hours -= 7.5
 		};
 		console.log('adjusting hours');
 		console.log($('#hours'));
@@ -39,15 +39,28 @@ var FivePointsEmployeesView = Backbone.View.extend({
 	addEmployee: function(event){
 		event.preventDefault();
 		console.log('Adding Employee');
+		var daysAvail = [];
+		var employeeName = '';
 		var $firstName = $(this.el).find('#firstName');
-		var $lastName = $(this.el).find('#lastName');	
-		this.collection.add({Name: ($firstName.val() + ' ' + $lastName.val())});
+		var $lastName = $(this.el).find('#lastName');
+		employeeName = $firstName.val() + ' ' + $lastName.val();	
+	//	this.collection.add({Name: ($firstName.val() + ' ' + $lastName.val())});
 		console.log('adding employee to select');
 		console.log($('#employeelist'));
 		$('#employeelist').append('<option value=' + $firstName.val() + ' ' + $lastName.val() + '>'
 			+ $firstName.val() + ' ' + $lastName.val() + '</option>');
+		var dayBoxes = document.getElementsByClassName('days');
+		for (day = 0; day < dayBoxes.length; day++){
+			if (dayBoxes[day].checked == true){
+				daysAvail.push(dayBoxes[day].id);
+			}
+		}
+		this.collection.add({Name: employeeName, Days: daysAvail});
 		$firstName.val('');
 		$lastName.val('');
+		for (day = 0; day < dayBoxes.length; day++){
+			dayBoxes[day].checked = false
+		}
 	},
 
 
