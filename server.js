e035c81a76d1223bb3bbee606751a475ddbf543c
Eric Gray <ericxgray@gmail.com>
2015-06-03 23:14:22 -0700

    Clean branch before tinkering

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res){
-	res.end("HI HI HI")
-})
+fs.readFile('./fivepoints.html', function (err, html){
+	if (err) {
+		throw err;
+	};
+	console.log(html);
+		http.createServer(function(req, res){
+			res.writeHeader(200, {"Content-Type": "text/html"});  
+		    res.write(html);
+			res.end();
+}).listen(process.env.PORT || 5000);
+});
+
 var portCall = process.env.PORT || 5000
-server.listen(process.env.PORT || 5000)
 console.log("Listening on port " + portCall)
 console.log(process.env);
 console.log(process.env.ORCHESTRATE_KEY);
