8d00f37e2175a28b119d3e9a3a9306db7f5ff58d
Eric Gray <ericxgray@gmail.com>
2015-06-04 09:16:52 -0700

    Server changes

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
+var path = require('path');
 
-fs.readFile('./fivepoints.html', function (err, html){
-	if (err) {
-		throw err;
-	};
-	console.log(html);
-		http.createServer(function(req, res){
-			res.writeHeader(200, {"Content-Type": "text/html"});  
-		    res.write(html);
-			res.end();
+http.createServer(function(request, response) {
+ 
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+  
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+ 
+    if (fs.statSync(filename).isDirectory()) filename += '/fivepoints.html';
+ 
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+ 
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
 }).listen(process.env.PORT || 5000);
-});
+//}).listen(process.env.PORT || 5000);
+
 
 var portCall = process.env.PORT || 5000
 console.log("Listening on port " + portCall)
