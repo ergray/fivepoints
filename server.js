54683e9dc9b08a1f516c7550f758aa74d26159a6
Eric Gray <ericxgray@gmail.com>
2015-06-03 19:25:22 -0700

    Changed to Herokus PORT

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var http = require('http');
 var server = http.createServer(function(req, res){
 	res.end("HI HI HI")
 })
-var portCall = process.argv[2] || 5000
-server.listen(process.argv[2] || 5000)
+var portCall = process.env.PORT || 5000
+server.listen(process.env.PORT || 5000)
 console.log("Listening on port " + portCall)
 console.log(process.env);
 console.log(process.env.ORCHESTRATE_KEY);
