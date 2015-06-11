8ee04b0635facca7a577fe2741920a1fd8283d7e
Eric Gray <ericxgray@gmail.com>
2015-06-10 17:02:20 -0700

    heroku adjustments

--- server.js
+++ server.js
@@ -6,75 +6,83 @@ var Router = require('routes');
 var router = Router();
 var db = require('orchestrate')(process.env.ORCHESTRATE_KEY);
 var dbName = "fivePointsEmployees";
+var date = Date.now();
+var countDown = 0;
 
 var sayHello = function(){
-	console.log("Hello say Hello")
-}
+	console.log("API is triggered" + date);
+	countDown++;
+	console.log(countDown);
+};
 
-var grabCollection = db.get(dbName, 'employees')
+var orchestrateDB = function(request, response){db.get(dbName, 'employees')
 	.then(function (result) {
-		console.log(result.body);
-		return result.body;
+		countDown++;
+		console.log("logged from grab success " + countDown);
+		console.log("Request and reponse");
+		console.log(JSON.stringify(result.body.fivepoints));
+		response.end(JSON.stringify(result.body.fivepoints));
 	})
 	.fail(function (err) {
+		countDown++;
+		console.log("logged from grab fail " + countDown);
 		console.log(err)
 		return;
-	});
+	})
+};
+	
+
+var grabCollection = function(request, response){
+	console.log("Here is request, response");
+	//console.log(request.body);
+	//console.log(response.body);
+	return orchestrateDB(request, response);
+};
 
 
-router.addRoute("/api", grabCollection)
-
+router.addRoute("/api", grabCollection);
 
 http.createServer(function(request, response) {
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
-    var match = router.match(uri);
-    console.log("Here is match");
-    if (match != undefined) {
-    	if (match.route == '/api'){
-    		match.fn.apply();
-  		}
-    	}
-    	console.log(match);
-    ;
-
-/*
-    var match = router.match(uri);
-    match.fn(req,res,match);
-  console.log(uri);
-  console.log("this is filename")
-  console.log(filename);
-
-  */
-
-
-  fs.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
-
- 
-    if (fs.statSync(filename).isDirectory()) filename += 'public/fivepoints.html';
- 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
- 
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
+	var uri = url.parse(request.url).pathname
+	, filename = path.join(process.cwd(), uri);
+	console.log("here is uri");
+	console.log(uri);
+	console.log("here is filename");
+	console.log(filename);
+	console.log("and here is what router.match sees");
+	console.log(router.match(uri));
+	var match = router.match(uri);
+	if (match != undefined){
+		match.fn(request, response)
+		//router.match(uri).fn.apply(null, {request: request}, {response: response});
+		return;
+	}
+	if (uri == "/"){
+		fs.readFile("./public/fivepoints.html", 'utf8', function(err, contents){
+			if (err) {
+				response.writeHead(500);
+				response.end(err);
+				return;
+			}
+			response.end(contents);
+		})
+	} else {
+		fs.readFile(filename, 'utf8', function(err, contents){
+			if (err) {
+				response.writeHead(500);
+				response.end(err);
+				return;
+			}
+			response.end(contents);
+		})
+	};
+	/*};
+	  fs.readFile(uri, function(err, contents){
+			if (err) {
+				response.writeHead(500);
+				response.end(err);
+				return;
+			}
+			response.end(contents);
+		});*/
 }).listen(process.env.PORT || 5000);
-//}).listen(process.env.PORT || 5000);
-
-
-var portCall = process.env.PORT || 5000
-console.log("Listening on port " + portCall)
